d6c632441ceb71d4565d58476fc8e3e11dec13ff
Tomas Martinez <to.martinezg@duocuc.cl>
2026-04-13 09:48:24 -0400

    Creacion de proyecto Springboot

---
+++ recomendacionApp/recomendacionApp/src/test/java/cl/FilmFlux/recomendacionApp/RecomendacionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.FilmFlux.recomendacionApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecomendacionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
