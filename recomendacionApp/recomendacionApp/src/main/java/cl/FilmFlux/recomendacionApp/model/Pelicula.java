735d3e7b4e758923b36fd5d9fbc3194e53be904d
Gustavomendezcrts <gu.mendezc@duocuc.cl>
2026-04-15 15:03:28 -0400

    Se crearon modelos de Series y Peliculas

---
+++ recomendacionApp/recomendacionApp/src/main/java/cl/FilmFlux/recomendacionApp/model/Pelicula.java
@@ -0,0 +1,31 @@
+package cl.FilmFlux.recomendacionApp.model;
+
+import java.sql.Date;
+
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pelicula {
+    @Id
+    @NotNull
+    private int id;
+    @NotBlank
+    private String imdb;
+    @NotBlank
+    private String titulo;
+    @NotBlank
+    private String director;
+    @NotBlank
+    private Date fechaEstreno;
+    @NotBlank
+    private String genero;
+    @NotNull
+    private int puntaje;
+}
