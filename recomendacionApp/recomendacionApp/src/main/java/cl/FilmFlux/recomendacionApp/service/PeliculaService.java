f72e62aecf158bb2b2c195f41c26fb7216d9f871
Gustavomendezcrts <gu.mendezc@duocuc.cl>
2026-04-23 12:19:57 -0400

    creacion del PeliculaService

---
+++ recomendacionApp/recomendacionApp/src/main/java/cl/FilmFlux/recomendacionApp/service/PeliculaService.java
@@ -0,0 +1,38 @@
+package cl.FilmFlux.recomendacionApp.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cl.FilmFlux.recomendacionApp.model.Pelicula;
+import cl.FilmFlux.recomendacionApp.repository.PeliculaRepository;
+
+@Service
+public class PeliculaService {
+    @Autowired
+    private PeliculaRepository peliculaRepository;
+
+    public List<Pelicula> getPeliculas(){
+        return peliculaRepository.findAll();
+    }
+
+    public Pelicula savePelicula(Pelicula peli){
+        return peliculaRepository.save(peli);
+    }
+
+    public Pelicula getPeliculaId(int id){
+        return peliculaRepository.findById(id).orElse(null);
+    }
+
+    public Pelicula updatPelicula(Pelicula peli){
+        if(!peliculaRepository.existsById(peli.getId())){
+            return null;
+        }
+        return peliculaRepository.save(peli);
+    }
+
+    public void deletePelicula(int id){
+        peliculaRepository.deleteById(id);
+    }
+}
