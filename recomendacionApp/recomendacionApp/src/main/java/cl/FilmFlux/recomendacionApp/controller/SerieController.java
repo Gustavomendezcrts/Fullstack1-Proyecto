a2edf624ba35ae6b050ef186c688778ccf270bce
Tomas Martinez <to.martinezg@duocuc.cl>
2026-06-03 14:48:20 -0400

    basurin

--- recomendacionApp/recomendacionApp/src/main/java/cl/FilmFlux/recomendacionApp/controller/SerieController.java
+++ recomendacionApp/recomendacionApp/src/main/java/cl/FilmFlux/recomendacionApp/controller/SerieController.java
@@ -9,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import cl.FilmFlux.recomendacionApp.model.Pelicula;
 import cl.FilmFlux.recomendacionApp.model.Serie;
 import cl.FilmFlux.recomendacionApp.service.SerieService;
 import jakarta.validation.Valid;
